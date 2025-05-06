8d43491d14a6bbb34900d4d55e771354dd81fd91
zaozao <1789401516qq@gmail.com>
2025-05-06 15:41:04 +0800

    2025-5-6 15:41

---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+    public float fireRate = 0.3f;
+    public float health = 10;
+    public int score = 100;
+    private BoundsCheck bndCheck;
+    public Vector3 pos
+    {
+        get
+        {
+            return this.transform.position;
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+
+    void Update()
+    {
+        Move();
+        if(bndCheck != null && bndCheck.offDown)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+}
--- Assets/_Scripts/Hero.cs
+++ Assets/_Scripts/Hero.cs
@@ -9,9 +9,28 @@ public class Hero : MonoBehaviour
     public float speed = 30;
     public float rollMult = -45;
     public float pitchMult = 30;
+    public float gameRestartDelay = 2f;
 
     [Header("Set Dynamically")]
-    public float shieldLevel = 1;
+    public float _shieldLevel = 1;
+    public GameObject lastTriggerGo = null;
+
+    public float shiedldLevel
+    {
+        get
+        {
+            return _shieldLevel;
+        }
+        set
+        {
+            shiedldLevel = Mathf.Min(value, 4);
+            if(value < 0)
+            {
+                Destroy(this.gameObject);
+                Main.S.DelayedRestart(gameRestartDelay);
+            }
+        }
+    }
     // Start is called before the first frame update
     private void Awake()
     {
@@ -36,5 +55,18 @@ public class Hero : MonoBehaviour
         pos.y += yAxis * speed * Time.deltaTime;
         transform.position = pos;
         transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        if (go == lastTriggerGo) return;
+        lastTriggerGo = go;
+        if(go.tag == "Enemy")
+        {
+            shieldLevel--;
+            Destroy(go);
+        }
     }
 }

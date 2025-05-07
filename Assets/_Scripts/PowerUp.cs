89cb15829f915534f0bcf4cb3e1e28797c6aba49
zaozao <1789401516qq@gmail.com>
2025-05-07 15:39:24 +0800

    2025-5-7 15:39

---
+++ Assets/_Scripts/PowerUp.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public Vector2 rotMinMax = new Vector2(15, 90);
+    public Vector2 driftMinMax = new Vector2(.25f, 2);
+    public float liftTime = 6f;
+    public float fadeTime = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
